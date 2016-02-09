55c4400cb316134f2d3220e5d4d1d2a573c14eda
lauradebella <lauradebella@gmail.com>
2016-02-09 14:52:24 -0200

    Teste 3 - conections

--- Node/main.js
+++ Node/main.js
@@ -16,6 +16,7 @@ http.createServer(function (request, response) {
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');*/
 
+/*code2
 var fs = require("fs");
 
 fs.readFile('input.txt', function (err, data) {
@@ -23,4 +24,31 @@ fs.readFile('input.txt', function (err, data) {
     console.log(data.toString());
 });
 
-console.log("Program Ended");
+console.log("Program Ended");*/
+
+
+// Import events module
+var events = require('events');
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function(){
+   console.log('data received succesfully.');
+});
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
